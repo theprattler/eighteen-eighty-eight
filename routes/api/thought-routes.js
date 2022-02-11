dd7e54edf50ff0f94ee8b8dc599e285925a17caa
The Prattler <jmichaelsmith@juno.com>
2022-02-11 09:40:51 -0500

    basic file structure of routes created

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+
+const {} = require('../../controllers/thought-controller');
+
+// /api/thoughts
+
+// /api/thoughts/:thoughtId/reactions
+
+module.exports = router;
---
+++ routes/api/user-routes.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+
+const {} = require('../../controllers/user-controller');
+
+// /api/users
+
+// /api/users/:usersId/friends/:friendId
+
+module.exports = router;
